1558ebe5fce47c1ec35a45dea3fe1052c3586cfe
Adam Herbst <12887762+AdamHerbst@users.noreply.github.com>
2020-01-18 19:44:42 -0500

    Very basic system functionality with SQLite up.
    We will be probably be writing our own main.java for this instead of modifying it and most likely using SQLite for our database.

---
+++ LibrarySystem/src/DBUsingJava.java
@@ -0,0 +1,33 @@
+import java.sql.*;
+
+public class DBUsingJava
+
+{
+
+public static void main( String args[] )
+
+{
+
+Connection c = null;
+
+try {
+
+Class.forName("org.sqlite.JDBC");
+
+c = DriverManager.getConnection("jdbc:sqlite:LibraryDB.db");
+
+}
+
+catch ( Exception e ) {
+
+System.err.println( e.getClass().getName() + ": " + e.getMessage() );
+
+System.exit(0);
+
+}
+
+System.out.println("database successfully created");
+
+}
+
+}
